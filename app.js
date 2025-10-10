52e3bacb156d57075f1c60b4e6dc57cf38862b64
Rosano Coutinho <github.com@xeari.com>
2025-10-10 11:05:43 +0100

    scaffold app

---
+++ app.js
@@ -0,0 +1,30 @@
+// remoteStorage module
+const remoteStorage = new RemoteStorage({
+  modules: [todos],
+});
+
+remoteStorage.access.claim('todos', 'rw');
+
+// setup after page loads
+document.addEventListener('DOMContentLoaded', () => {
+
+  (new Widget(remoteStorage)).attach('widget-wrapper');
+
+  remoteStorage.on('ready', () => {
+    if (!remoteStorage.remote.token) {
+      return;
+    }
+
+    Array.from(document.querySelectorAll('project iframe')).forEach(e => {
+      const parent = e.parentElement;
+      e.remove()
+
+      const source = e.src;
+      e.src = `${ source }#remotestorage=${ remoteStorage.remote.userAddress }&access_token=${ remoteStorage.remote.token }`.replace(/#+/g, '#');
+      parent.appendChild(e);
+      
+      setTimeout(() => e.src = source, 1000);
+    });
+  });
+  
+});
---
+++ remotestorage-module.js
@@ -0,0 +1,3 @@
+const todos = {
+  name: 'todos',
+};
